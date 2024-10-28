02e313b035d11cece4e237e4e4feb8673031cd1f
me <me@arch.localdomain>
2024-10-27 19:21:06 -0500

    initial commit

--- lua/me/color.lua
+++ lua/me/color.lua
@@ -1,3 +1,5 @@
+vim.opt.number = true
+vim.opt.relativenumber = true
 --vim.cmd('colorscheme elflord')
 vim.api.nvim_create_autocmd('ColorScheme', {
   callback = function()
@@ -11,9 +13,15 @@ vim.api.nvim_create_autocmd('ColorScheme', {
       'SignColumn',
       'EndOfBuffer',
       'TablineFill', -- this is specific to how I like my tabline to look like
+      'LineNr',
     }
     for _, name in pairs(highlights) do vim.cmd.highlight(name .. ' guibg=none ctermbg=none') end
 end,
 })
 
 vim.cmd('colorscheme ' .. (vim.g.colors_name or 'onedark'))
+
+
+vim.api.nvim_set_hl(0, 'LineNrAbove', { fg='grey', bold = false })
+vim.api.nvim_set_hl(0, 'LineNr', { fg='blue', bold = false })
+vim.api.nvim_set_hl(0, 'LineNrBelow', { fg='grey', bold = false })
